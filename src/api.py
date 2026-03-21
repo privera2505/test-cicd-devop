0a388310d243ee13b2742ed313f6b5bc3ddbc393
privera <p.riverah@uniandes.edu.co>
2026-03-22 00:17:41 +0200

    fix

--- src/api.py
+++ src/api.py
@@ -2,3 +2,14 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+@app.get("/api2/test1")
+async def payment_webhook(request: Request):
+    return "Test micro 123"
+
+@app.get("/api2/test2")
+async def payment_webhook(request: Request):
+    return "Test micro 123"
+
+@app.get("/api2/health")
+async def payment_webhook(request: Request):
+    return 200
