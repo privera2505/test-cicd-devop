f24a5eac97dd8746bb799216040bc38087b8408b
privera <p.riverah@uniandes.edu.co>
2026-03-22 18:16:25 +0200

    add test and test config to cloudbuild.yaml

---
+++ tests/test_api.py
@@ -0,0 +1,12 @@
+import pytest
+from fastapi.testclient import TestClient
+from src.api import app
+
+@pytest.fixture
+def client():
+    """Crea un cliente de prueba"""
+    return TestClient(app)   
+
+def test_heath(client):
+    """test unique"""
+    assert True == True
