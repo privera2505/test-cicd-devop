34c73e0d124722a4181243ee3937fadc24170470
privera <p.riverah@uniandes.edu.co>
2026-03-28 22:42:38 +0100

    add

--- src/api.py
+++ src/api.py
@@ -4,11 +4,11 @@ app = FastAPI()
 
 @app.get("/api2/test1")
 async def payment_webhook(request: Request):
-    return "Test micro 123"
+    return "Test micro 124"
 
 @app.get("/api2/test2")
 async def payment_webhook(request: Request):
-    return "Test micro 123"
+    return "Test micro 124"
 
 @app.get("/api2/health")
 async def payment_webhook(request: Request):
