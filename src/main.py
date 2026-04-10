7cb24516624f23663ab83068af15aa7f78fe2a04
privera <p.riverah@uniandes.edu.co>
2026-04-10 17:31:50 -0500

    fix

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import uvicorn
 from os import getenv
 
 API_HOST = getenv("API_HOST", "0.0.0.0")
-PORT = getenv("PORT", "8000")
+API_PORT = getenv("API_PORT", "8000")
 TEST_NAME = getenv("TEST_NAME", "Api1.1")
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host=API_HOST, port=int(PORT))
+    uvicorn.run("api:app", host=API_HOST, port=int(API_PORT))
