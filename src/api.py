6105b34e4ef5b1c7d87121b4a294d9aed3e5e9b2
privera <p.riverah@uniandes.edu.co>
2026-04-10 00:23:07 -0500

    Fix para probar funcionamiento variables de ambiente

--- src/api.py
+++ src/api.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI, Request
+from main import TEST_NAME
 
 app = FastAPI()
 
-#@app.get("/api2/test1")
-#async def payment_webhook(request: Request):
-#    return "Test micro 124"
-#
-#@app.get("/api2/test2")
-#async def payment_webhook(request: Request):
-#    return "Test micro 124"
-#
-#@app.get("/api2/health")
-#async def payment_webhook(request: Request):
-#    return 200
+@app.get("/api2/test1")
+async def payment_webhook(request: Request):
+    return f"Test micro 124 {TEST_NAME}"
+
+@app.get("/api2/test2")
+async def payment_webhook(request: Request):
+    return f"Test micro 124 {TEST_NAME}"
+
+@app.get("/api2/health")
+async def payment_webhook(request: Request):
+    return 200
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from os import getenv
 
 API_HOST = getenv("SIDECAR_HOST", "0.0.0.0")
 API_PORT = getenv("SIDECAR_PORT", "8000")
+TEST_NAME = getenv("TEST_NAME", "Api1.1")
 
 if __name__ == "__main__":
     uvicorn.run("api:app", host=API_HOST, port=int(API_PORT))
